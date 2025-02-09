ab69bf3baabafc212ecf57314a77e162f3d1957e
Shaikh Mohammad Umar Farooq <shaikhumar06122001@gmail.com>
2025-02-09 14:46:50 +0530

    test lint-staged 2

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Welcome to Pizzical.");
+function welcome(name: string) {
+  console.log("Welcome to Pizzical.");
+
+  const user = { name: "john" };
+
+  const fname = user.name;
+
+  return name + fname;
+}
+
+welcome("Doe");
