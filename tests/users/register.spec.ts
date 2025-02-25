99e375e670b61e7b70597060397557af6ef136b5
Shaikh Mohammad Umar Farooq <shaikhumar06122001@gmail.com>
2025-02-25 15:18:04 +0530

    feat(test): implement initial tests for user registration endpoint.

---
+++ tests/users/register.spec.ts
@@ -0,0 +1,59 @@
+import request from "supertest";
+import app from "../../src/app";
+
+describe("POST /auth/register", () => {
+  describe("Given all fields", () => {
+    it("should return 201 status code", async () => {
+      // Arrange
+      const userData = {
+        firstName: "John",
+        lastName: "Doe",
+        email: "johndoe@email.com",
+        password: "secret",
+      };
+
+      // Act
+      const response = await request(app).post("/auth/register").send(userData);
+
+      // Assert
+      expect(response.statusCode).toBe(201);
+    });
+
+    it("should return valid json response", async () => {
+      // Arrange
+      const userData = {
+        firstName: "John",
+        lastName: "Doe",
+        email: "johndoe@email.com",
+        password: "secret",
+      };
+
+      // Act
+      const response = await request(app).post("/auth/register").send(userData);
+
+      // Assert: application/json in the response header content-type
+      expect(response.headers["content-type"]).toEqual(
+        expect.stringContaining("json"),
+      );
+    });
+
+    it("should persist the user in the database", async () => {
+      // Arrange
+      const userData = {
+        firstName: "John",
+        lastName: "Doe",
+        email: "johndoe@email.com",
+        password: "secret",
+      };
+
+      // Act
+      const response = await request(app).post("/auth/register").send(userData);
+
+      // Assert: application/json in the response header content-type
+      expect(response.headers["content-type"]).toEqual(
+        expect.stringContaining("json"),
+      );
+    });
+  });
+  describe("Fields are missing", () => {});
+});
