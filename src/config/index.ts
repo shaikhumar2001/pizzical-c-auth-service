6fc57e08541cb4c93dad332f048044d2d5a7c1f4
Shaikh Mohammad Umar Farooq <shaikhumar06122001@gmail.com>
2025-02-27 12:53:35 +0530

    feat(database): setup database. - setup typeorm with respect to postgreSQL. - make environment variable dynamic. - use cross-env for environment variable injection across all operating systems.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,16 @@
 import { config } from "dotenv";
-config();
+import path from "path";
+config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) });
 
-const { NODE_ENV, PORT } = process.env;
+const { NODE_ENV, PORT, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } =
+  process.env;
 
 export const Config = {
   NODE_ENV,
   PORT,
+  DB_HOST,
+  DB_PORT,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_NAME,
 };
