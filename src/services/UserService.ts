ef6d34554bbf96277e818ea3a61ff37e361bb819
Shaikh Mohammad Umar Farooq <shaikhumar06122001@gmail.com>
2025-08-20 20:07:33 +0530

    feat(user): implement password hashing and email uniqueness validation in registration.

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,18 +1,29 @@
 import { Repository } from "typeorm";
 import { User } from "../entity/User";
 import { UserData } from "../types";
+import bcrypt from "bcrypt";
 import createHttpError from "http-errors";
 import { Roles } from "../constants";
 
 export class UserService {
   constructor(private userRepository: Repository<User>) {}
   async create({ firstName, lastName, email, password }: UserData) {
+    // Check if the email already exists
+    const user = await this.userRepository.findOne({ where: { email: email } });
+    if (user) {
+      const err = createHttpError(400, "Email already exists!");
+      throw err;
+    }
+
+    // Hash the password
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
     try {
       return await this.userRepository.save({
         firstName,
         lastName,
         email,
-        password,
+        password: hashedPassword,
         role: Roles.CUSTOMER,
       });
     } catch {
--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -111,6 +111,46 @@ describe("POST /auth/register", () => {
       expect(users[0]).toHaveProperty("role");
       expect(users[0].role).toBe(Roles.CUSTOMER);
     });
+
+    it("should store hashed password in the database", async () => {
+      // Arrange
+      const userData = {
+        firstName: "John",
+        lastName: "Doe",
+        email: "johndoe@email.com",
+        password: "secret",
+      };
+
+      // Act
+      await request(app).post("/auth/register").send(userData);
+
+      // Assert
+      const userRepository = connection.getRepository(User);
+      const users = await userRepository.find();
+      expect(users[0].password).not.toBe(userData.password);
+      expect(users[0].password).toHaveLength(60);
+      expect(users[0].password).toMatch(/^\$2[ayb]\$\d+\$/);
+    });
+
+    it("should return 400 status code if email already exists", async () => {
+      // Arrange
+      const userData = {
+        firstName: "John",
+        lastName: "Doe",
+        email: "johndoe@email.com",
+        password: "secret",
+      };
+      const userRepository = connection.getRepository(User);
+      await userRepository.save({ ...userData, role: Roles.CUSTOMER });
+
+      // Act
+      const response = await request(app).post("/auth/register").send(userData);
+
+      // Assert
+      const users = await userRepository.find();
+      expect(response.statusCode).toBe(400);
+      expect(users).toHaveLength(1);
+    });
+    describe("Fields are missing", () => {});
   });
-  describe("Fields are missing", () => {});
 });
