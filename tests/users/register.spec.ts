737732a2595e5d824429aab611c28ef90e52c077
Shaikh Mohammad Umar Farooq <shaikhumar06122001@gmail.com>
2025-08-21 11:30:21 +0530

    feat(auth(: add email sanitization via express-validator

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -172,4 +172,24 @@ describe("POST /auth/register", () => {
       expect(users).toHaveLength(0);
     });
   });
+  describe("Fields are not in proper format", () => {
+    it("should trim the email field", async () => {
+      // Arrange
+      const userData = {
+        firstName: "John",
+        lastName: "Doe",
+        email: " johndoe@email.com ",
+        password: "secret",
+      };
+
+      // Act
+      await request(app).post("/auth/register").send(userData);
+
+      // Assert
+      const userRepository = connection.getRepository(User);
+      const users = await userRepository.find();
+      const user = users[0];
+      expect(user.email).toBe("johndoe@email.com");
+    });
+  });
 });
